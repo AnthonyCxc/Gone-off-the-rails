9b1bfebdf193cb549000e22ad7870f65bc09d808
AnthonyCxc <AnthonyCxc95@gmail.com>
2017-11-08 15:50:56 +0800

    second commit

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # README
 
+Add the gems that you include here.
+-gem 'figaro'
+-gem 'faker'
+-gem 'omni-auth'
+-gem 'omni-auth google'
+-gem 'omni-auth facebook'
+-gem ''
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
@@ -22,3 +30,5 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+
--- db/migrate/20171108072925_create_users.rb
+++ db/migrate/20171108072925_create_users.rb
@@ -1,20 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-<<<<<<< HEAD:db/migrate/20171108043417_create_users.rb
     	t.string :name
     	t.string :email
     	t.string :password
     	t.string :address
-    	t.column :gender :integer, default: 3 # 3 = other, 1 = female , 2 = male
+    	t.column :gender,:integer, default: 3 # 3 = other, 1 = female , 2 = male
     	t.date :date_of_birth
-    	t.column :pto_role :integer, default: 0 # 0 = normal user, 1 = pto user
+    	t.column :pto_role,:integer, default: 0 # 0 = normal user, 1 = pto user
     	
     	t.timestamps
-=======
-
-      t.timestamps
->>>>>>> 9ace36bf11fa4fe5fdaeb3d5bccc446cc3b90f59:db/migrate/20171108072925_create_users.rb
     end
   end
 end
