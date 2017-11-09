2255b0c920101e51fcb24c043d83d17f01272ab2
opstech23 <bbody8215@gmail.com>
2017-11-09 11:07:32 +0800

    user login works with devise, bootstrap navbar added

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108073048) do
+ActiveRecord::Schema.define(version: 20171108205720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admin_users", force: :cascade do |t|
     t.string "admin_name"
-    t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "project_causes", force: :cascade do |t|
@@ -57,12 +68,23 @@ ActiveRecord::Schema.define(version: 20171108073048) do
   end
 
   create_table "pto_users", force: :cascade do |t|
-    t.string "email"
     t.integer "approved_by"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["approved_by"], name: "index_pto_users_on_approved_by"
+    t.index ["email"], name: "index_pto_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_pto_users_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_pto_users_on_user_id"
   end
 
@@ -78,7 +100,6 @@ ActiveRecord::Schema.define(version: 20171108073048) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password"
     t.string "address"
     t.integer "gender", default: 3
@@ -86,6 +107,18 @@ ActiveRecord::Schema.define(version: 20171108073048) do
     t.integer "pto_role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
